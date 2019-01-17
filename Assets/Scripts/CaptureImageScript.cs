7f4e1c06670a212a9686fab6ec262c3965b2a06e
Paritosh <contact@iamparitoshsharma.me>
2019-01-18 03:51:20 +0530

    Added Android Native Sharing

---
+++ Assets/Scripts/CaptureImageScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CaptureImageScript : MonoBehaviour {
+
+    public RawImage rawImage;
+
+    public static string filename;
+
+    public static string ImageName(int width, int height)
+    {
+        return string.Format("{0}/image_{1}x{2}_{3}.png",
+                             Application.dataPath,
+                             width, height,
+                             System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    }
+
+    public void imageCapture()
+    {
+        Texture2D image = rawImage.texture as Texture2D;
+        byte[] bytes = image.EncodeToPNG();
+        filename = ImageName(image.width, image.height);
+        System.IO.File.WriteAllBytes(filename, bytes);
+        SceneManager.LoadScene("picshare");
+    }
+}
